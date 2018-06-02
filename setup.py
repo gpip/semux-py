83227093f045ab76b920868a86b600f10d7160c2
Guilherme Polo <hello@ggpolo.com>
2018-06-02 13:46:46 -0500

    repo url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='semux',
     version='0.0.1',
+    url='https://github.com/gpip/semux-py',
     packages=find_packages(),
     install_requires=[
       'PyNaCl==1.2.1',
